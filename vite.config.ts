6ba97fca39cfa35a7ada3c2692927b0976967990
Joanadayse <daysejoana3@gmail.com>
2025-11-03 15:00:18 -0300

    ajuste de build

--- vite.config.ts
+++ vite.config.ts
@@ -1,37 +1,42 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
-import svgr from "vite-plugin-svgr";
-import dts from "vite-plugin-dts";
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import svgr from 'vite-plugin-svgr';
+import dts from 'vite-plugin-dts';
+import path from 'path';
 
 export default defineConfig({
   plugins: [
     react(),
     svgr({
+      include: '**/*.svg',
       svgrOptions: {
-        icon: true, 
+        exportType: 'default',
+        icon: true,
       },
-      include: "**/*.svg",
     }),
     dts({
       insertTypesEntry: true,
-      outDir: "dist",
-      tsconfigPath: "./tsconfig.build.json",
+      tsconfigPath: './tsconfig.build.json',
+      outDir: 'dist/types',
     }),
   ],
   build: {
     lib: {
-      entry: "./src/index.ts",
-      name: "StyledKit",
+      entry: path.resolve(__dirname, 'src/index.ts'),
+      name: 'StyledKit',
+      formats: ['es', 'cjs'], // importante gerar os dois formatos
       fileName: (format) => `styled-kit.${format}.js`,
     },
     rollupOptions: {
-      external: ["react", "react-dom"],
+      external: ['react', 'react-dom'],
       output: {
         globals: {
-          react: "React",
-          "react-dom": "ReactDOM",
+          react: 'React',
+          'react-dom': 'ReactDOM',
         },
       },
     },
+    outDir: 'dist',
+    sourcemap: true,
   },
 });
